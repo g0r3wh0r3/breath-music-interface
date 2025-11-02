b114f6b5b23c9273b71b93461a1d41187489cf81
g0r3wh0r3 <nrkjosep@uwaterloo.ca>
2025-11-02 17:41:41 -0500

    update

--- testing/ind_calc.py
+++ testing/ind_calc.py
@@ -1,12 +1,8 @@
 from math import pi
-c = 3.3E-6
-period = 12.2E-9
-f = 1/period 
-print(f)
-sqrt_lc = 1/(2*pi*f)
-print(sqrt_lc)
-lc = sqrt_lc**2 
-print(lc)
-l = lc/c
-
-print(l)
+# calculates the inductance from the frequency 
+freq = float(input())
+#period = float(input())
+#freq = 1/period
+C = 3.3E-6 # capacitor val 
+L = 1/((2*pi*freq)**2**C)
+print(L)
--- testing/testing.md
+++
@@ -1,1 +0,0 @@
-this is where notes on testing the device should go. write down things like supply voltage used, results, and take pictures 
